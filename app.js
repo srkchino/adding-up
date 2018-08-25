cad4f9c4b814f9281bd5c3ffbd5194cc767870f9
srkchino <srk.chino@gmail.com>
2018-08-25 01:41:46 +0000

    降順に変更。出力の表示桁数揃え。

--- app.js
+++ app.js
@@ -38,10 +38,14 @@ rl.on('close', () => {
         value.change = value.popu15 / value.popu10;
     }
     const rankingArray = Array.from(map).sort((pair1,pair2) => {
-        return pair2[1].change - pair1[1].change;
+        return pair1[1].change - pair2[1].change;
     });
-    const rankingStrings = rankingArray.map((pair) => {
-        return pair[0] + ': ' + pair[1].popu10 + '=> ' + pair[1].popu15 + ' 変化率: ' + pair[1].change;
+    const rankingStrings = rankingArray.map((pair, i) => {
+        const rank = ('00' + (i + 1)).slice(-2);
+        const todofuken = ('　　　　' + pair[0]).slice(-4);
+        const p1 = ('       ' + pair[1].popu10).slice(-7);
+        const p2 = ('       ' + pair[1].popu15).slice(-7);
+        return  rank + '位 ' + todofuken + ': ' + p1 + ' => ' + p2 + '  変化率: ' + pair[1].change;
     });
     console.log(rankingStrings);
 });
